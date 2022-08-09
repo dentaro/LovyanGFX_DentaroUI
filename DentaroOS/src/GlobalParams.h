323fc2251e470f292d261bb3cea15468445ea47f
dentaro <77978725+dentaro@users.noreply.github.com>
2022-08-09 13:48:33 +0900

    Delete DentaroOS directory

--- DentaroOS/src/GlobalParams.h
+++
@@ -1,8 +0,0 @@
-#ifndef _GLOBALPARAMS_H_
-#define _GLOBALPARAMS_H_
-
-struct GlobalParams {
-  static constexpr char* preferName       = (char*)"LovyanLauncher";
-  static constexpr char* preferKeyLastBin = (char*)"LastBinFileName";
-};
-#endif
